8c83a68dae7752c5fc93f65e5ce75910801d756e
Jakkrit Junrat <ball_vong_yo_gh@hotmail.com>
2019-02-27 10:49:08 +0700

    Change vmss url

--- webhook.js
+++ webhook.js
@@ -12,7 +12,7 @@ webHooks.add('webhooksite', 'https://webhook.site/1ccd7a86-6955-4b46-92f0-142f94
     console.log(err)
 });
 
-webHooks.add('msteam4vmss', 'https://outlook.office.com/webhook/e5919028-b41e-4efb-bfec-62564e26becf@1bf12a4f-2277-4d37-83db-525780663f76/IncomingWebhook/2922d7754e884e678a3b5564fa0c107b/b89b96b3-7b9a-4d34-ae36-e83156f15335').then(function(){
+webHooks.add('msteam4vmss', 'https://outlook.office.com/webhook/e5919028-b41e-4efb-bfec-62564e26becf@1bf12a4f-2277-4d37-83db-525780663f76/IncomingWebhook/5fa4b23d5cf2422fb191264b0eba8938/b89b96b3-7b9a-4d34-ae36-e83156f15335').then(function(){
 // done
 }).catch(function(err){
     console.log(err)
